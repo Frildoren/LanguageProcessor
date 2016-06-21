10ad967bc3a9fdf0a877bf7df42fab65f26afe59
Roberto Garrido <garrihendrix@gmail.com>
2016-06-21 12:45:31 +0200

    enums added (I think all of them)

--- src/enums/TokenType.java
+++ src/enums/TokenType.java
@@ -4,5 +4,43 @@ public enum TokenType {
 
     UNDEFINED,
 
+    // Arithmetics
+    ADD,
+    SUB,
+    MULTIPLIES,
+    DIVIDES,
+    MODULUS,
+
+    // Relationals
+    EQUAL,
+    INEQUAL,
+    LESS,
+    GREATER,
+    LESS_EQUAL,
+    GREATER_EQUAL,
+
+    // Logical
+    AND,
+    OR,
+    NEGATE,
+    BITWISE_AND_ASSIGN,
+
+    // Punctuation
+    OPEN_BRACKET,
+    CLOSE_BRACKET,
+    OPEN_CURLY_BRACKET,
+    CLOSE_CURLY_BRACKET,
+    COMMA,
+    SEMICOLON,
+
+    // Others
+    TRUE,
+    FALSE,
+    VAR,
+    CHAR,
+    BOOLEAN,
+    STRING,
+    INTEGER,
+    END,
 
 }
