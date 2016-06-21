d9b3ab27e4f532db47a31094b4b52c64f90fd285
Roberto Garrido <garrihendrix@gmail.com>
2016-06-21 13:41:38 +0200

    Initial push

--- src/analyzers/Tokenizer.java
+++ src/analyzers/Tokenizer.java
@@ -4,6 +4,7 @@ import exceptions.PDLException;
 import structures.Token;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
 
@@ -15,8 +16,18 @@ public class Tokenizer extends BufferedReader {
         super(in);
     }
 
-    public void process() throws PDLException {
+    public void process() throws PDLException, IOException {
         //ToFo: Medium
+
+        String state = "";
+        int value = -1, lineCounter = 1;
+
+        while((value = read()) != -1) {
+
+            
+
+        }
+
     }
 
     public List<Token> getTokens(){
