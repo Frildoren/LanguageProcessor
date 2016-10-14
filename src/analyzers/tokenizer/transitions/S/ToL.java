2d8e9eda34212c0883cde5d12ca39741fca0c47b
Roberto Garrido <garrihendrix@gmail.com>
2016-10-14 10:27:27 +0200

    Added transition S to L

--- src/analyzers/tokenizer/transitions/S/ToL.java
+++ src/analyzers/tokenizer/transitions/S/ToL.java
@@ -1,7 +1,19 @@
 package analyzers.tokenizer.transitions.S;
 
+import analyzers.tokenizer.transitions.Transition;
+import enums.TokenType;
+import structures.Token;
+
 /**
  * Created by garri on 14/10/16.
  */
-public class ToL {
+public class ToL extends Transition{
+    @Override
+    public String inputRule() { return "!"; }
+
+    @Override
+    public Token semanticRules() {
+        getTokenizer().read();
+        return new Token(TokenType.OP_LOGIC_NEGATION);
+    }
 }
