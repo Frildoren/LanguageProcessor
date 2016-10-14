f97a1be9ad4c07380ac0c458de01a489e724409e
Diego Fernández <diegofpb93@gmail.com>
2016-10-14 10:07:44 +0200

    Transition F To G.

---
+++ src/analyzers/tokenizer/transitions/F/ToG.java
@@ -0,0 +1,23 @@
+package analyzers.tokenizer.transitions.F;
+
+import analyzers.tokenizer.TokenizerImpl;
+import analyzers.tokenizer.transitions.Transition;
+import enums.TokenType;
+import structures.Token;
+
+public class ToG extends Transition {
+    public ToG(TokenizerImpl tokenizer) {
+        super(tokenizer);
+    }
+
+    @Override
+    public String inputRule() {
+        return "\"";
+    }
+
+    @Override
+    public Token semanticRules() {
+        getTokenizer().read();
+        return new Token(TokenType.CONSTANT_STRING);
+    }
+}
