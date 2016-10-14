402e6ff74863b614dbef359c99f0d40a165ab292
Diego Fernández <diegofpb93@gmail.com>
2016-10-14 10:07:55 +0200

    Transition S To D.

---
+++ src/analyzers/tokenizer/transitions/S/ToD.java
@@ -0,0 +1,24 @@
+package analyzers.tokenizer.transitions.S;
+
+import analyzers.tokenizer.TokenizerImpl;
+import analyzers.tokenizer.transitions.Transition;
+import structures.Token;
+
+public class ToD extends Transition {
+
+    public ToD(TokenizerImpl tokenizer) {
+        super(tokenizer);
+    }
+
+    @Override
+    public String inputRule() {
+        return "\\d";
+    }
+
+    @Override
+    public Token semanticRules() {
+        getTokenizer().toState('D');
+        getTokenizer().read();
+        return null;
+    }
+}
