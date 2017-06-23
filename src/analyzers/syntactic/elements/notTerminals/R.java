b88c3c1e179b68bc9bdb2fb3d920edf9f9f27281
Pablo Garcia <yinxro@gmail.com>
2017-06-23 18:33:10 +0200

    Added some missing not terminal elements

---
+++ src/analyzers/syntactic/elements/notTerminals/R.java
@@ -0,0 +1,23 @@
+package analyzers.syntactic.elements.notTerminals;
+
+import analyzers.syntactic.elements.Element;
+import analyzers.syntactic.elements.NotTerminalElement;
+import analyzers.syntactic.elements.terminals.Lambda;
+import analyzers.syntactic.elements.terminals.TokenElement;
+import enums.TokenType;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class R extends NotTerminalElement {
+
+    @Override
+    public List<List<Element>> getBranchesClasses() {
+        // R -> + E | < E | lambda
+        return Arrays.asList(
+                Arrays.asList(new Element[]{ new TokenElement(TokenType.OP_ARITHMETIC_ADD), new E() }),
+                Arrays.asList(new Element[]{ new TokenElement(TokenType.OP_RELATIONAL_SMALLER_THAN), new E() }),
+                Arrays.asList(new Element[]{ new Lambda() })
+        );
+    }
+}
