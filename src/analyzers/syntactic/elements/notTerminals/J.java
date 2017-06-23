18262a7afe26d22d2cf76f3ae816d17b33c04625
Diego Fernández <diegofpb93@gmail.com>
2017-06-23 18:30:31 +0200

    J

---
+++ src/analyzers/syntactic/elements/notTerminals/J.java
@@ -0,0 +1,31 @@
+package analyzers.syntactic.elements.notTerminals;
+
+import analyzers.syntactic.elements.Element;
+import analyzers.syntactic.elements.NotTerminalElement;
+import analyzers.syntactic.elements.terminals.TokenElement;
+import enums.TokenType;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class J extends NotTerminalElement {
+    @Override
+    public List<List<Element>> getBranchesClasses() {
+        //J -> Z J | B Z J | break Z J | I
+        return Arrays.asList(
+                Arrays.asList(new Element[]{
+                        new Z(),
+                        new J()}),
+                Arrays.asList(new Element[]{
+                        new B(),
+                        new Z(),
+                        new J()}),
+                Arrays.asList(new Element[]{
+                        new TokenElement(TokenType.BREAK),
+                        new Z(),
+                        new J()}),
+                Arrays.asList(new Element[]{
+                        new I()})
+        );
+    }
+}
