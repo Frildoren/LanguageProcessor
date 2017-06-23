2210ca1d8da961327decd1a549324e0ebb9f95d7
Diego Fernández <diegofpb93@gmail.com>
2017-06-23 18:28:57 +0200

    I

---
+++ src/analyzers/syntactic/elements/notTerminals/I.java
@@ -0,0 +1,26 @@
+package analyzers.syntactic.elements.notTerminals;
+
+import analyzers.syntactic.elements.Element;
+import analyzers.syntactic.elements.NotTerminalElement;
+import analyzers.syntactic.elements.terminals.Lambda;
+import analyzers.syntactic.elements.terminals.TokenElement;
+import enums.TokenType;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class I extends NotTerminalElement {
+    @Override
+    public List<List<Element>> getBranchesClasses() {
+        //I -> case int_const : J | lambda
+        return Arrays.asList(
+                Arrays.asList(new Element[]{
+                        new TokenElement(TokenType.CASE),
+                        new TokenElement(TokenType.CONSTANT_INTEGER),
+                        new TokenElement(TokenType.COLON),
+                        new J()}),
+                Arrays.asList(new Element[]{
+                        new Lambda()})
+        );
+    }
+}
