52c3c2b0a70d37e012cd92087b7499756649f3f5
Diego Fernández <diegofpb93@gmail.com>
2017-06-23 18:17:05 +0200

    B

---
+++ src/analyzers/syntactic/elements/notTerminals/B.java
@@ -0,0 +1,41 @@
+package analyzers.syntactic.elements.notTerminals;
+
+import analyzers.syntactic.elements.Element;
+import analyzers.syntactic.elements.NotTerminalElement;
+import analyzers.syntactic.elements.terminals.TokenElement;
+import enums.TokenType;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class B extends NotTerminalElement {
+    @Override
+    public List<List<Element>> getBranchesClasses() {
+        //B -> var T id | if ( E ) S | switch ( E ) | { Z I Z } | S
+        return Arrays.asList(
+                Arrays.asList(new Element[]{
+                        new TokenElement(TokenType.VAR),
+                        new T(),
+                        new TokenElement(TokenType.IDENTIFIER)}),
+                Arrays.asList(new Element[]{
+                        new TokenElement(TokenType.IF),
+                        new TokenElement(TokenType.OPEN_PARENTHESIS),
+                        new E(),
+                        new TokenElement(TokenType.CLOSE_PARENTHESIS),
+                        new S()}),
+                Arrays.asList(new Element[]{
+                        new TokenElement(TokenType.SWITCH),
+                        new TokenElement(TokenType.OPEN_PARENTHESIS),
+                        new E(),
+                        new TokenElement(TokenType.CLOSE_PARENTHESIS)}),
+                Arrays.asList(new Element[]{
+                        new TokenElement(TokenType.OPEN_BRACE),
+                        new Z(),
+                        new I(),
+                        new Z(),
+                        new TokenElement(TokenType.CLOSE_BRACE)}),
+                Arrays.asList(new Element[]{
+                        new S()})
+        );
+    }
+}
