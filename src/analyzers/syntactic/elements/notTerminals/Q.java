9d431527a42640619ae62050b3649ed74cd43f0b
Diego Fernández <diegofpb93@gmail.com>
2017-06-23 18:25:58 +0200

    Q

---
+++ src/analyzers/syntactic/elements/notTerminals/Q.java
@@ -0,0 +1,25 @@
+package analyzers.syntactic.elements.notTerminals;
+
+import analyzers.syntactic.elements.Element;
+import analyzers.syntactic.elements.NotTerminalElement;
+import analyzers.syntactic.elements.terminals.Lambda;
+import analyzers.syntactic.elements.terminals.TokenElement;
+import enums.TokenType;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Q extends NotTerminalElement {
+    @Override
+    public List<List<Element>> getBranchesClasses() {
+        //Q -> , E Q | lambda
+        return Arrays.asList(
+                Arrays.asList(new Element[]{
+                        new TokenElement(TokenType.COMMA),
+                        new E(),
+                        new Q()}),
+                Arrays.asList(new Element[]{
+                        new Lambda()})
+        );
+    }
+}
