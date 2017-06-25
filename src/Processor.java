ac6e8506ecedbc31f13043bae8a79f3a0a82bfbb
Pablo Garcia <yinxro@gmail.com>
2017-06-25 11:23:12 +0200

    Use file input

--- src/Processor.java
+++ src/Processor.java
@@ -1,8 +1,12 @@
+import analyzers.syntactic.SyntacticAnalizer;
+import analyzers.syntactic.SyntacticAnalyzerImpl;
 import analyzers.tokenizer.Tokenizer;
 import analyzers.tokenizer.TokenizerImpl;
 import exceptions.PDLException;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.StringReader;
 
 public class Processor {
@@ -10,15 +14,16 @@ public class Processor {
     public static void main(String[] args){
 
         try {
-            BufferedReader reader = new BufferedReader(new StringReader("84"));
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
             Tokenizer tokenizer = new TokenizerImpl(reader);
+            SyntacticAnalizer syntacticAnalizer = new SyntacticAnalyzerImpl(tokenizer);
 
-            while(true) {
-                p(tokenizer.readToken().toString());
-            }
+            syntacticAnalizer.process();
 
         } catch (PDLException e) {
             p(e.getMessage());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
     }
